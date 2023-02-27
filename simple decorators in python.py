49d668c295f75a648a7fe125ea3be9be44f3ca45
yixue <v7e0b@ugrad.cs.ubc.ca>
2023-02-26 21:29:50 -0800

    1

---
+++ simple decorators in python.py
@@ -0,0 +1,23 @@
+user = {"username": "jose", "access_level": "admin"}
+
+
+def get_admin_password():
+    return "1234"
+
+
+# solution without decorator:
+def secure_get_admin():
+    if user["access_level"] == "admin":
+        return "1234"
+
+
+def make_secure(func):
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+
+    return secure_function
+
+
+get_admin_password = make_secure(get_admin_password)
+print(get_admin_password())
